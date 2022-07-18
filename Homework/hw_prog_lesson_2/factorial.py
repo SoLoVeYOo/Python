12caef1413f1816f070ebbcbc9dbd55f784d9815
Dmitry <solovushkov.d@yandex.ru>
2022-07-18 19:50:51 +0400

    hw_prog_lesson_2

---
+++ Homework/hw_prog_lesson_2/factorial.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+vvodnoe = input('Введите число: ')
+
+def factorial (number: str) -> list:
+    proizvedenie = 1
+    result = []
+    for i in range(1, int(number) + 1):
+        proizvedenie *= i
+        result.append(proizvedenie) 
+    return result
+
+try:
+    vvodnoe = int(vvodnoe)
+    print(factorial(vvodnoe))
+except: print('Введите целое число')

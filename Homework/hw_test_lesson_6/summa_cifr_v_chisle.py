2099e2073aeefb9341f58ada220d705c06c64066
Dmitry <solovushkov.d@yandex.ru>
2022-08-04 23:54:50 +0400

    hw_lesson_6_p2

--- Homework/hw_test_lesson_6/summa_cifr_v_chisle.py
+++ Homework/hw_test_lesson_6/summa_cifr_v_chisle.py
@@ -4,16 +4,14 @@ vvodnoe = input('Введите число: ')
 
 def summa_v_chisle(chislo: str) -> int:
     summa = 0
-    temp = chislo.split(".")
-    temp1 = temp[0]
-    temp2 = temp[1] 
-    for i in range(len(temp1)): 
-            summa += int(temp1[i])
-    for i in range(len(temp2)): 
-            summa += int(temp2[i])        
+    chislo = chislo.replace('.', '') 
+    for i in range(len(chislo)): 
+        summa += int(chislo[i])   
     return summa
 
 try:
     proverka = float(vvodnoe)
     print(f'Сумма цифр в числе {vvodnoe} равна: {summa_v_chisle(vvodnoe)}')
 except: print('Введите вещесвенное число без ошибки')
+
+

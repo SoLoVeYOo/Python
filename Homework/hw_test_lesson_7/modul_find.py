3c319fb08939ef49c254c3635bb83e292e9ecbaa
Dmitry Solovushkov <soloveyxiii@yandex.ru>
2022-08-10 12:45:11 +0300

    hw_lesson_7_done

--- Homework/hw_test_lesson_7/modul_find.py
+++ Homework/hw_test_lesson_7/modul_find.py
@@ -3,11 +3,10 @@ import csv
 def find_number(last_name):
     with open(r'C:\Users\solov\OneDrive\Документы\GeekBrains\python\Homework\hw_test_lesson_7\data_base.csv', encoding='utf-8') as file:
         filereader = csv.reader(file, delimiter=",")
-
-    temp = False
-    for r in filereader:
-        if r[0] == last_name:
-            temp, tel = True, r[2]
-    if temp == True:
-        return tel
-    else: return 'Данной фамилии в записной книжке нет'
+        temp = False
+        for row in filereader:
+            if row[0] == last_name:
+                temp, tel = True, row[2]
+        if temp == True:
+            return tel
+        else: return 'номер отсутствует, либо данной записи в телефонной книге нет'

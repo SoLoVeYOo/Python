f97e5e3335e11da4c564be1c6cf0df952720a523
Dmitry <solovushkov.d@yandex.ru>
2022-10-13 23:47:43 +0400

    hw_test_lesson_8

--- Homework/hw_test_lesson_8/modul_add.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def add_info(first_name, last_name, phone_number, klass, pol):
-    try:
-        with open(r'Homework\hw_test_lesson_8\data_base.csv', 'a', encoding='utf-8') as file:
-                a = csv.writer(file, delimiter = ',', lineterminator='\r')
-                a.writerow([f'{first_name}', f'{last_name}', f'{phone_number}', f'{klass}', f'{pol}'])
-        return True
-    except: return False
